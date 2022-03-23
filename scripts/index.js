e264f6721eb465078632a02a5307ce10c519b277
Amon <anriarudoragon@gmail.com>
2022-03-23 08:24:29 +0200

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,5 @@ document.getElementById("branches-cities-number").innerHTML = randomNumber(20, 1
 document.getElementById("flights-today-number").innerHTML = randomNumber(5, 300);
 document.getElementById("todays-country").innerHTML = randomCountry();
 document.getElementById("todays-price").innerHTML = randomNumber(9, 75) + " " + randomNumber(100, 999)
+document.getElementById("hotels-count-n1").innerHTML = randomNumber(10, 80);
+document.getElementById("hotels-count-n2").innerHTML = randomNumber(100, 999);
